230a9fb25e951361302473575eb7bb4e0f49e333
Jeff Risberg <jrisberg@aol.com>
2012-04-25 18:35:33 -0700

    added event handlers

--- web-app/extjsapp/app/controller/common/AccountController.js
+++ web-app/extjsapp/app/controller/common/AccountController.js
@@ -2,5 +2,28 @@
  * Controller for the Account panel.
  */
 Ext.define('GR06.controller.common.AccountController', {
-    extend: 'Ext.app.Controller'
+    extend: 'Ext.app.Controller',
+    
+    refs: [
+     { ref: 'accountPanelGrid',
+       selector: 'accountPanel > gridpanel[itemId=accountGrid]' }
+    ],
+    
+    doRowClick: function() {
+        alert("click");
+    },
+    doRowDblClick: function() {
+        alert("dbl");
+    },
+    
+    init: function() {
+      alert("setting up event handlers");     
+      this.control({
+         'accountPanel > gridpanel': {
+             itemclick: this.doRowClick,  
+             itemdblclick: this.doRowDblClick                 
+         }
+         });
+      alert("setting up event handlers");      
+    }
 });
--- web-app/extjsapp/app/view/common/AccountPanel.js
+++ web-app/extjsapp/app/view/common/AccountPanel.js
@@ -3,11 +3,11 @@ Ext.define('GR06.view.common.AccountPanel', {
     alias: 'widget.accountPanel',    
 
     requires: [
-        'GR06.store.common.AccountStore'
+        'GR06.store.common.AccountStore',
+        'Ext.grid.RowSelectionModel'
     ],
     
-    title: 'Accounts',
-    id: 'AccountPanel',
+    title: 'Accounts',    
     layout: {
         align: 'stretch',
         padding: '',
@@ -16,32 +16,67 @@ Ext.define('GR06.view.common.AccountPanel', {
     border: 0,
     padding: '',   
     bodyPadding: 5,      
-
+   
     initComponent: function() {
         var me = this;
 
         Ext.applyIf(me, {
             items: [               
                 {
-                    xtype: 'grid',
+                    flex: 1,
+                    xtype: 'gridpanel',
+                    itemId: "accountGrid",
                     columns: [        
                         { dataIndex: 'name', text: 'Name', align:'left', width: 200 },
-                        { dataIndex: 'description', text: 'Description', align:'left' }
-                     
-                    ],
-                    store: Ext.create('GR06.store.common.AccountStore'),
-                    flex: 1
+                        { dataIndex: 'description', text: 'Description', align:'left' }                     
+                    ],                                      
+                    store: Ext.create('GR06.store.common.AccountStore')                    
                 },
                 {
-                    xtype: 'label',
-                    margin: '0 0 2 0',
-                    text: 'Account',
-                    flex: 0
+                    flex: 1,
+                    xtype: 'form',
+                    margin: '0 0 2 0',                                       
+                    defaultType: 'textfield',
+                    
+                    items: [{
+                        fieldLabel: 'Name',
+                        name: 'name',
+                        allowBlank: false
+                    },{
+                        fieldLabel: 'Description',
+                        name: 'description',
+                        allowBlank: false
+                    }],
+
+                    // Reset and Submit buttons
+                    buttons: [{
+                        text: 'Reset',
+                        handler: function() {
+                            this.up('form').getForm().reset();
+                        }
+                    },{
+                        text: 'Submit',
+                        formBind: true, //only enabled once the form is valid
+                        disabled: true,
+                        handler: function() {
+                            var form = this.up('form').getForm();
+                            if (form.isValid()) {
+                                form.submit({
+                                    success: function(form, action) {
+                                       Ext.Msg.alert('Success', action.result.msg);
+                                    },
+                                    failure: function(form, action) {
+                                        Ext.Msg.alert('Failed', action.result.msg);
+                                    }
+                                });
+                            }
+                        }
+                    }]                  
                 }
             ]
         });
 
         me.callParent(arguments);
     }
-
 });
+
